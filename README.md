018f1e1edd16441ee27643d5eb54d8d315ed6b79
Neelkumar Amin <neelkumar.amin@outlook.com>
2020-01-20 22:32:45 -0500

    Bind socket to host and set up connection queue

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# python-scripts
+# Random Python scripts
--- Sockets/Server.py
+++ Sockets/Server.py
@@ -1,5 +1,17 @@
+# A network socket is an internal endpoint for sending or receiving data within a node on a computer network.
 import socket
 
-#AF_INET is an address family corresponding to Internet Protocol version 4 (IPv4).
-#SOCK_STREAM means that this is a Transmission Control Protocol (TCP) socket.
+# AF_INET is an address family corresponding to Internet Protocol version 4 (IPv4).
+# SOCK_STREAM means that this is a Transmission Control Protocol (TCP) socket.
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind socket to tuple of IP and port.
+# Using local IP of host and ephemeral port.
+s.bind((socket.gethostname(), 4242))
+
+# Prepare to listen and limit connections with a queue of two.
+s.listen(2)
+
+# Listen continuously for client socket object.
+
+##> Insert Code
